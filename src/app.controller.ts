ce15e478d8755407041400fafcfd8e0760257586
chenxuanfeng <chenxuanfeng@meituan.com>
2022-03-29 14:26:08 +0800

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+
+// 使用@Controller装饰器来定义控制器, @Get是请求方法的装饰器，对getHello方法进行修饰， 表示这个方法会被GET请求调用。
 
 @Controller()
 export class AppController {
@@ -7,6 +9,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    // 我们可以看出使用 @Injectable 修饰后的 AppService, 在 AppModule 中注册之后，在 app.controller.ts 中使用，我们就不需要使用 new AppService() 去实例化，直接引入过来就可以用
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+// Nest.js 的思维方式一开始确实不容易理解，但假如你接触过AngularJS，就会感到熟悉，如果你用过 Java 和 Spring 的话，就可能会想，这不是抄的 Spring boot嘛
+// AngularJS、Spring 和 Nest.js 都是基于控制反转原则设计的,而且都使用了依赖注入的方式来解决解耦问题
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
+  exports: [], // 导出服务的列表，供其他模块导入使用。如果希望当前模块下的服务可以被其他模块共享，需要在这里配置导出
+  controllers: [AppController], // 处理 http 请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给 providers 处理
+  providers: [AppService], // Nest.js 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享（注入器的概念后面依赖注入部分会讲解）
 })
-export class AppModule {}
+export class AppModule {} // AppModule 是应用程序的根模块，根模块提供了用来启动应用的引导机制，可以包含很多功能模块
