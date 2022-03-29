fa160077eb86389ef9a842a58b639b190cceeca3
chenxuanfeng <chenxuanfeng@meituan.com>
2022-03-29 15:19:14 +0800

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 // 使用@Controller装饰器来定义控制器, @Get是请求方法的装饰器，对getHello方法进行修饰， 表示这个方法会被GET请求调用。
@@ -7,9 +7,23 @@ import { AppService } from "./app.service";
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("hello")
   getHello(): string {
     // 我们可以看出使用 @Injectable 修饰后的 AppService, 在 AppModule 中注册之后，在 app.controller.ts 中使用，我们就不需要使用 new AppService() 去实例化，直接引入过来就可以用
     return this.appService.getHello();
+  }
+
+  // 2.通配符路径(?+* 三种通配符 )
+  // 可以匹配到 get请求, http://localhost:3000/app/user_xxx
+  @Get("user_*")
+  getUser() {
+    return "getUser";
+  }
+
+  // 3.带参数路径
+  // 可以匹配到put请求，http://localhost:3000/app/list/xxxx
+  @Post("list/:id")
+  update() {
+    return "update";
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 设置swagger文档
+  const config = new DocumentBuilder()
+    .setTitle("管理后台")
+    .setDescription("管理后台接口文档")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+  // 我们就可以访问：http://localhost:3000/docs,此时就能看到Swagger生成的文档
+
   await app.listen(3000);
 }
+
 bootstrap();
